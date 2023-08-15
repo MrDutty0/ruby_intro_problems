9c5e80a3e0d110f15d48103827ed0683bab3eee8
Tomas Civilis <96726777+MrDutty0@users.noreply.github.com>
2023-08-15 13:36:05 +0300

    Refactor stock picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,20 +1,21 @@
-def stock_picker(stock_price = []) 
+def stock_picker(stock_price) 
   ans = []
-  left = 0
-  biggest_profit = -2**63
+  buy_idx = 0
+  biggest_profit = 0
 
   stock_price.each_with_index do |price, idx|
-    curr_profit = price - stock_price[left]
-    bought_price = stock_price[left]
+    curr_profit = price - stock_price[buy_idx]
 
     if curr_profit > biggest_profit
-      ans = [left, idx]
+      ans = [buy_idx, idx]
       biggest_profit = curr_profit 
     end
 
-    if price < bought_price
-      left = idx
+    if price < stock_price[buy_idx]
+      buy_idx = idx
     end
   end
   ans
 end
+
+stock_picker([17,3,6,9,15,8,6,1,10]) # => [1,4] 
